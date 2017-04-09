cfd5fb959043ebc1a11db75f7cf6aa9476cfe420
Mikkel Oscar Lyderik Larsen <m@moscar.net>
2017-04-09 23:19:21 +0200

    Comment/document lookup code

--- pass/disk.go
+++ pass/disk.go
@@ -37,6 +37,7 @@ func defaultStorePath() (string, error) {
 	return filepath.EvalSymlinks(path)
 }
 
+// lookup will find sites matching or partly matching the passed domain.
 func lookup(domain string, sites []*site) []*site {
 	results := make([]*site, 0)
 	domainParts := reverse(strings.Split(domain, "."))
@@ -47,6 +48,7 @@ func lookup(domain string, sites []*site) []*site {
 		}
 	}
 
+	// sort by length of domain, longest first.
 	sort.Slice(results, func(i, j int) bool {
 		return len(results[i].domain) > len(results[j].domain)
 	})
@@ -54,6 +56,7 @@ func lookup(domain string, sites []*site) []*site {
 	return results
 }
 
+// reverse reverses the element order of a string slice.
 func reverse(strings []string) []string {
 	for i, j := 0, len(strings)-1; i < j; i, j = i+1, j-1 {
 		strings[i], strings[j] = strings[j], strings[i]
@@ -62,18 +65,26 @@ func reverse(strings []string) []string {
 	return strings
 }
 
-func subMatch(x, y []string, min int) bool {
-	if len(y) < min {
+// subMatch validates whether the candidate string slice matches at least min
+// parts of the query slice.
+// Example:
+//    query = ["org", "example", "my"]
+//    candidate = ["org", "example"]
+//    min = 2
+// this will match because both elements of candidate is found in the same
+// order in query.
+func subMatch(query, candidate []string, min int) bool {
+	if len(candidate) < min {
 		return false
 	}
 
-	if len(x) < len(y) {
+	if len(query) < len(candidate) {
 		return false
 	}
 
 	matches := 0
-	for i := len(y) - 1; i > -1; i-- {
-		if y[i] == x[i] {
+	for i := len(candidate) - 1; i > -1; i-- {
+		if candidate[i] == query[i] {
 			matches++
 		} else {
 			return false
@@ -87,17 +98,27 @@ func subMatch(x, y []string, min int) bool {
 	return false
 }
 
+// site defines a domain and the related users stored for this domain.
 type site struct {
 	domain string
 	users  []string
 }
 
-func (s *diskStore) Lookup(query string) ([]string, error) {
+// Lookup looks up domains in the password store based on the domainQuery string.
+// the lookup will return a list of domains/subdomains matching the query with
+// the most precise domain first.
+// Assuming the domains "sub1.domain.tld", "sub2.domain.tld" and "domain.tld"
+// are defined in the store, and the query domain is "sub1.domain.tld" the
+// lookup will return the list ["sub1.domain.tld", "domain.tld"]. If
+// "domain.tld" is the query domain then only ["domain.tld"] will be returned
+// as the matching is done from front to end thus not matching the subdoamins.
+func (s *diskStore) Lookup(domainQuery string) ([]string, error) {
 	sites := make([]*site, 0)
 	siteCh := make(chan *site)
 	errCh := make(chan error)
 
 	go func() {
+		// use FastWalk to collect all domains/users defined in the password store.
 		err := fastwalk.FastWalk(s.path, func(dir string, typ os.FileMode) error {
 			if dir == s.path {
 				return nil
@@ -130,7 +151,7 @@ func (s *diskStore) Lookup(query string) ([]string, error) {
 		return nil, err
 	}
 
-	sites = lookup(query, sites)
+	sites = lookup(domainQuery, sites)
 
 	results := make([]string, 0, len(sites))
 	for _, site := range sites {
